00a56ffb7566cd015e15c7f2ad8ae8c440c6c1fe
Anant Bansal <202151021@iiitvadodara.ac.in>
2024-07-24 15:18:34 +0530

    new dir commit

--- OA_Practice/MW_1.cpp
+++ OA_Practice/MW_1.cpp
@@ -1,10 +1,10 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define ll long
+typedef long ll;
 
 long maxBeauty(int n, vector<int> from, vector<int> to, vector<int> a){
     vector<vector<ll>> tree(n);
-    for(ll i = 1; i < n; i++){
+    for(ll i = 0; i < n - 1; i++){
         tree[from[i] - 1].push_back(to[i] - 1);
         tree[to[i] - 1].push_back(from[i] - 1);
     }
@@ -20,27 +20,41 @@ long maxBeauty(int n, vector<int> from, vector<int> to, vector<int> a){
         }
     };
     dfs(0, -1, 0);
-
-    function<void(ll, ll, ll)> reroot = [&](ll node, ll par, ll prev) {
-
+    ll sum = accumulate(a.begin(), a.end(), 0LL);
+    function<void(ll, ll)> reroot = [&](ll node, ll par) {
+        for(auto it : tree[node]){
+            if(it == par) continue;
+            ll curr = ans[node] - sub[it] + sum - sub[it];
+            ans[it] = curr;
+            reroot(it, node);
+        }
     };
-    reroot(0, -1, 0);
-
-    
+    reroot(0, -1);
     return *max_element(ans.begin(), ans.end());
 }
 
 int main(){
-    int n, m; cin >> n >> m;
-    vector<int> from(m), to(m);
-    for(int i = 0; i < m; i++){
-        int u, v; cin >> u >> v;
-        from[i] = u;
-        to[i] = v;
-    }
-    int N; cin >> N;
-    vector<int> a(N);
-    for(auto &it : a) cin >> it;
 
-    cout << maxBeauty(n, from, to, a);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+    #ifndef ONLINE_JUDGE 
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif 
+    int t; cin >> t;
+    while(t--){
+        int n, m; cin >> n >> m;
+        vector<int> from(m), to(m);
+        for(int i = 0; i < m; i++){
+            int u, v; cin >> u >> v;
+            from[i] = u;
+            to[i] = v;
+        }
+        int N; cin >> N;
+        vector<int> a(N);
+        for(auto &it : a) cin >> it;
+
+        cout << maxBeauty(n, from, to, a) << "\n";
+    }
+    return 0;
 }
